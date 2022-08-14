b358d78abb7f2883ba5f8109d1e2832e1edf33d5
Suraj <1507surajkuamr@gmail.com>
2022-08-14 21:18:48 +0530

    commits

--- FILEREADER_WRITER/D.java
+++ FILEREADER_WRITER/D.java
@@ -8,7 +8,7 @@ public class D {
         try {
             File f = new File("E://Suraj.txt");
             FileWriter w = new FileWriter(f);
-            w.write("Suraj");//write("we can write anything inside double");
+            w.write("Suraj Kumar");//write("we can write anything inside double");
             w.close();//we dont write this it will not go inside the file
 
             System.out.println("Done ! file lenght is: "+f.length());
