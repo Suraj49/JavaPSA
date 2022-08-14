4eca566442bb4a0bfd4523c2af52b0cc0a37d696
Suraj <1507surajkuamr@gmail.com>
2022-08-14 21:14:49 +0530

    commits

---
+++ FILEREADER_WRITER/D.java
@@ -0,0 +1,19 @@
+package FILEREADER_WRITER;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class D {
+    public static void main(String[] args) {
+        try {
+            File f = new File("E://Suraj.txt");
+            FileWriter w = new FileWriter(f);
+            w.write("Suraj");//write("we can write anything inside double");
+            w.close();//we dont write this it will not go inside the file
+
+            System.out.println("Done ! file lenght is: "+f.length());
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
