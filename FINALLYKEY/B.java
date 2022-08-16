a7c05ffdfb0a536caae515c8e609339b2b7f5ea1
Suraj <1507surajkuamr@gmail.com>
2022-08-16 18:50:49 +0530

    commits

---
+++ FINALLYKEY/B.java
@@ -0,0 +1,11 @@
+package FINALLYKEY;
+
+public class B {
+    public static void main(String[] args) {
+        try{
+            System.out.println("try block");
+        }finally {
+            System.out.println("finally block");
+        }
+    }
+}
