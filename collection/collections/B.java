7cc606744d5d3dbe38ebe33e431945d05ff6041f
Suraj <1507surajkuamr@gmail.com>
2022-09-03 00:46:15 +0530

    commits

--- collection/collections/B.java
+++ collection/collections/B.java
@@ -2,6 +2,7 @@ package collection.collections;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 
 public class B {
     public static void main(String[] args) {
@@ -16,5 +17,15 @@ public class B {
         int idx=Collections.binarySearch(a,20);//to search any element in list we must need sorted list;
         System.out.println(a);
         System.out.println(idx);
+        LinkedList l=new LinkedList();
+        l.add(1547);
+        l.add(5421);
+        l.add(215);
+        l.add(546);
+        l.add(546);
+        l.add(1513);
+        Collections.sort(l);
+        int index=Collections.binarySearch(l,2);//if element is not present then it will return -1;
+        System.out.println(index);
     }
 }
